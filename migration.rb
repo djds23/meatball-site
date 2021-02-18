1bbff2b8dd4526c93b29ae5f68e5e0a3acf126af
Dean Silfen <dean.silfen@gmail.com>
2021-02-18 18:50:19 -0500

    add edit date and attempt to get sorting working

--- migration.rb
+++ migration.rb
@@ -10,7 +10,13 @@ end
 file_path = "source/data/links.yml"
 
 data = YAML.load(read_file(file_path))
+ref_date = Time.now
 
-data["recipes"] = data["recipes"].reverse
+data["recipes"] = data["recipes"].map do |h|
+  edited_at = ref_date - 1
+  h["editedAt"] = edited_at.round(0).to_s
+  ref_date = edited_at
+  h
+end
 
 File.write(file_path, YAML.dump(data))
